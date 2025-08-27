ee7a31419cac5907b4412fec95ad022f3ac98120
Jhon <jfrojas1997@gmail.com>
2025-08-27 17:05:50 -0500

    update: se mejora el obtener todos los items agregando datos necesarios para visualizacion mas facil mediante dashboard

--- back-end/controllers/order_item.js
+++ back-end/controllers/order_item.js
@@ -4,7 +4,16 @@ const path = require('path');
 
 exports.getAllOrder_items = async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM order_items");
+    const result = await pool.query(`
+      SELECT 
+      oi.id_order,
+      p.name_product,
+      SUM(oi.quantity) AS total_cantidad,
+      SUM(oi.quantity * p.price) AS total_producto
+      FROM order_items oi
+      INNER JOIN products p ON oi.id_product = p.id_product
+      GROUP BY oi.id_order, p.id_product, p.name_product
+      ORDER BY oi.id_order, total_producto DESC;`);
     res.json(result.rows);
   } catch (error) {
     console.error("Error al obtener items de orden:", error);
