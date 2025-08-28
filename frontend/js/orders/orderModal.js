e5432495999ea8a9d3a83dd73bcbac0c9e3fccef
Jhonatan <jhonatanmorales99@gmail.com>
2025-08-27 20:57:16 -0500

    feat: cargar modal con items de la compra

--- frontend/js/orders/orderModal.js
+++ frontend/js/orders/orderModal.js
@@ -1,17 +1,18 @@
-import { DETAILS_API } from "./url_orders.js";
+import { ORDERS_API } from "./url_orders.js";
 
 export const orderModals = {
     async open(id) {
         const detailsTable = document.getElementById("detailsTable");
+        detailsTable.innerHTML = ""
         try {
-            const res = await fetch(DETAILS_API+id);
+            const res = await fetch(`${ORDERS_API}${id}/items`);
             const details = await res.json();
             details.forEach(detail => {
                 const row = document.createElement("tr");
                 row.innerHTML = `
                     <th>${detail.quantity}</th>
-                    <td>${detail.product_name}</td>
-                    <td>${detail.total}</td>
+                    <td>${detail.name_product}</td>
+                    <td>${detail.total_producto}</td>
                 `;
                 detailsTable.appendChild(row)
             });
--- frontend/js/orders/url_orders.js
+++ frontend/js/orders/url_orders.js
@@ -1,3 +1,2 @@
 export const ORDERS_API = "http://localhost:3001/orders/";
 
-export const DETAILS_API = "http://localhost:3001/order_items/"
