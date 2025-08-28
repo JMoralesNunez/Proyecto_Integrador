a0809f5e33c00a49765deb7bab4cacea09a17d2a
Jhon <jfrojas1997@gmail.com>
2025-08-27 20:08:39 -0500

    add: se grega endpoint para ver los items asociados a cada orden

--- back-end/routes/orders.js
+++ back-end/routes/orders.js
@@ -5,6 +5,7 @@ const ordersController = require('../controllers/orders');
 router.get('/', ordersController.getAllOrders);
 router.get('/numberOrders', ordersController.getNumberOrders);
 router.get('/totalOrders', ordersController.getTotalOrders);
+router.get('/:id/items', ordersController.getItemsByOrderId);
 router.get('/:id', ordersController.getOrderById);
 router.post('/', ordersController.createOrder);
 router.delete('/:id', ordersController.deleteOrder);
